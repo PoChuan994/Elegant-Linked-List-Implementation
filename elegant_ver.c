6d9f7bdc433de541077bc564f09490725cdc4faa
PoChuan994 <62919709+PoChuan994@users.noreply.github.com>
2024-12-19 16:50:52 +0800

    Update elegant_ver.c

--- elegant_ver.c
+++ elegant_ver.c
@@ -1,3 +1,23 @@
-int main(){
-  
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 15      // the number of nodes
+
+struct Node{
+    int val;
+    struct Node *nxt;
+}
+typedef struct Node Node;
+
+struct List{
+    Node *head;
+}
+typedef struct List List;
+
+Node **search(List *list, Node *target){
+    Node **indirect = &list->head;
+    while(*indirect && *indirect!=target){
+        indirect = &(*indirect)->nxt;
+    }
+    return indirect;
 }
